66a37628bf7e76c202b3e0389d28d87b78b912a1
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-08-04 15:18:34 -0400

    Agregando MultiOAuth2AuthenticationProcessingFilter

---
+++ src/main/java/cl/trackthor/security/OAuth2FilterConfig.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.trackthor.security;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.servlet.Filter;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.springframework.web.filter.CompositeFilter;
+
+/**
+ *
+ * @author matia
+ */
+@Configuration
+public class OAuth2FilterConfig {
+    
+    @Bean(name = "oauth2AuthenticationProcessingFilter")
+    public Filter oauth2AuthenticationProcessingFilter() {
+        // Lista de filtros
+        ArrayList<AuthenticationManager> authenticationManagers = new ArrayList<>();
+        authenticationManagers.add(googleManager());
+        authenticationManagers.add(facebookManager());
+        
+        MultiOAuth2AuthenticationProcessingFilter filter = new MultiOAuth2AuthenticationProcessingFilter();
+        filter.setAuthenticationManagers(authenticationManagers);
+        
+        return filter;
+    }
+    
+    public OAuth2AuthenticationManager googleManager() {
+        
+        // OAuth2ProtectedResourceDetails
+        AuthorizationCodeResourceDetails resourceDetails = new AuthorizationCodeResourceDetails();
+        resourceDetails.setClientId("76115053642-l3g6sm2lbt8m4q154nnr19vp4meirf1v.apps.googleusercontent.com");
+        resourceDetails.setClientSecret("OwyLiRQScUYBQ5wnVZ_Laz9l");
+        resourceDetails.setAccessTokenUri("https://www.googleapis.com/oauth2/v4/token");
+        resourceDetails.setAuthenticationScheme(AuthenticationScheme.query);
+        resourceDetails.setUserAuthorizationUri("https://accounts.google.com/o/oauth2/v2/auth");
+        resourceDetails.setClientAuthenticationScheme(AuthenticationScheme.header);
+        resourceDetails.setScope(Arrays.asList("email", "profile"));
+        resourceDetails.setUseCurrentUri(false);
+        
+        // OAuth2RestTemplate
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails);
+        
+        // ResourceServerTokenServices
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(
+                "https://www.googleapis.com/oauth2/v3/userinfo",
+                resourceDetails.getClientId()
+        );
+        tokenServices.setRestTemplate(restTemplate);
+        
+        // AuthenticationManager
+        OAuth2AuthenticationManager manager = new OAuth2AuthenticationManager();
+        manager.setTokenServices(tokenServices);
+        
+        return manager;
+    }
+    
+    public OAuth2AuthenticationManager facebookManager() {
+        
+        // OAuth2ProtectedResourceDetails
+        AuthorizationCodeResourceDetails resourceDetails = new AuthorizationCodeResourceDetails();
+        resourceDetails.setClientId("360755857937906");
+        resourceDetails.setClientSecret("26756d4c686e7b3b716c06ea9de218d4");
+        resourceDetails.setAccessTokenUri("https://graph.facebook.com/v2.8/oauth/access_token");
+        resourceDetails.setAuthenticationScheme(AuthenticationScheme.query);
+        resourceDetails.setUserAuthorizationUri("https://www.facebook.com/v2.8/dialog/oauth");
+        resourceDetails.setClientAuthenticationScheme(AuthenticationScheme.header);
+        resourceDetails.setScope(Arrays.asList("public_profile", "email"));
+        resourceDetails.setUseCurrentUri(false);
+        resourceDetails.setTokenName("oauth_token");
+        
+        // OAuth2RestTemplate
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails);
+        
+        // ResourceServerTokenServices
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(
+                "https://graph.facebook.com/me?fields=id,name,email",
+                resourceDetails.getClientId()
+        );
+        tokenServices.setRestTemplate(restTemplate);
+        
+        // AuthenticationManager
+        OAuth2AuthenticationManager manager = new OAuth2AuthenticationManager();
+        manager.setTokenServices(tokenServices);
+        
+        return manager;
+    }
+    
+}
--- src/main/java/cl/trackthor/security/SecurityConfig.java
+++ src/main/java/cl/trackthor/security/SecurityConfig.java
@@ -5,37 +5,29 @@
  */
 package cl.trackthor.security;
 
-import cl.trackthor.service.CustomOAuth2UserService;
+import javax.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
+    @Qualifier("oauth2AuthenticationProcessingFilter")
     @Autowired
-    private CustomOAuth2UserService userService;
+    private Filter filter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic()
 			.and().csrf().disable()
-			.authorizeRequests()
-                                .antMatchers("/login/**")
-                                    .permitAll()
-				.anyRequest()
-					.permitAll()
-				.and()
-					.oauth2Login()
-						.userInfoEndpoint()
-							.userService(userService);
+			.authorizeRequests().anyRequest().permitAll()
+                        .and().addFilterBefore(filter, BasicAuthenticationFilter.class);
     }
 
 }
